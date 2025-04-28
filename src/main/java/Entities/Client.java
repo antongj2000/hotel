20ad696f3085c2d791839a8043beb2a6b3f990fd
antongj2000 <anton29070@gmail.com>
2025-04-28 15:50:29 +0200

    Initial commit

---
+++ src/main/java/Entities/Client.java
@@ -0,0 +1,16 @@
+package Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Client {
+
+	@Id
+	@GeneratedValue
+	public Long id;
+
+	public String field;
+
+}
