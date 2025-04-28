98ebad5805a2ec5e2b7b21a800826414dda4be53
antongj2000 <anton29070@gmail.com>
2025-04-28 16:52:23 +0200

    This is my  first oneTomany relation
    Relation between Hotel and Room

---
+++ src/main/java/Entities/Hotel.java
@@ -0,0 +1,50 @@
+package Entities;
+
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Hotel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "hotel_seq")
+	private Long id;
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Hotel " + name;
+	}
+	
+	   @OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL, orphanRemoval = true)
+	    private List<Room> rooms;
+
+	
+
+}
---
+++ src/main/java/Entities/Room.java
@@ -0,0 +1,46 @@
+package Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Room {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "room_seq")
+	private Long id;
+	private String name;
+	private int roomNumber;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "hotel_id", nullable = false)
+	private Hotel hotel;
+
+}
