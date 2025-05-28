bc7c5340a00ba60ed63e2ec3a4eb0fef86376336
antongj2000 <anton29070@gmail.com>
2025-05-28 15:05:29 +0200

    treci domaci

--- src/main/java/restclient/CountryClient.java
+++ src/main/java/restclient/CountryClient.java
@@ -9,9 +9,11 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
 import Client.CountryResponse;
 
+@Path("/v3")
 @RegisterRestClient(configKey = "country-api")
 public interface CountryClient {
 
+	@Path("/availableCountries")
 	@GET
 	List<CountryResponse> getCountries(@QueryParam("countryCode") String countryCode, @QueryParam("name") String name);
 }
